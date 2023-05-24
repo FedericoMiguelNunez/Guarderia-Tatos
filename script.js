7f7b5ab769d1b437388090d8a650616301052daf
FedericoMiguelNunez <fede24896@gmail.com>
2023-05-24 13:57:10 -0300

    se emprolijo el script y se puso en una carpeta

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-
-const Continuar = document.getElementById("continuar");
+const errorNombre = document.getElementsByClassName("Error-nombre")[0];
+errorNombre.style.display = "none";
+const errorMichi = document.getElementsByClassName("Error-michi")[0];
+errorMichi.style.display = "none";
 function Saludo(event) {
     event.preventDefault();
-    console.log("anda todo bien");
-    const nombre = document.getElementById("Nombre").value;
-    cambio.innerHTML = `hola${nombre}`;
-  
-}
-Continuar.addEventListener("click", Saludo);
+            const nombre = document.getElementById("Nombre").value;
+            const gato = document.getElementById('Michi').value;
+            if (nombre.length < 1 ){
+                
+                errorNombre.style.display = "block";
+                errorMichi.style.display = "none";
+            }
+            else if(gato.length < 1){
+                errorMichi.style.display = "block";
+                errorNombre.style.display = "none";
+            }
+            else{
+          
+            window.location.href = `Guarderia-Tatos.html?nombre=${nombre}&gato=${gato}`;
+        }
+        }
+        
+        const Continuar = document.getElementById("continuar");
+        Continuar.addEventListener("click", Saludo);
--- script2.js
+++ script2.js
@@ -1,1 +1,5 @@
 const cambio = document.getElementById("cambiar");
+const params = new URLSearchParams(window.location.search);
+const nombre = params.get('nombre');
+const gato = params.get('gato');
+cambio.innerHTML = `¡Bienvenidos <br> ${nombre} y ${gato}!`;
