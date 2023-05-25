53a7369cf62300165c9c1fd5d311715a07bc596a
FedericoMiguelNunez <fede24896@gmail.com>
2023-05-25 13:42:43 -0300

    se esta modificando  fundadores 1.0

--- script2.js
+++ script2.js
@@ -1,5 +1,12 @@
 const cambio = document.getElementById("cambiar");
+const DondeTuGato = document.getElementById("donde-tu-gato");
+const pRecibimos = document.getElementById("p-recibimos");
+const AyelenText = document.getElementById("ayelen-Text");
 const params = new URLSearchParams(window.location.search);
 const nombre = params.get('nombre');
 const gato = params.get('gato');
 cambio.innerHTML = `¡Bienvenidos <br> ${nombre} y ${gato}!`;
+DondeTuGato.innerHTML = `Hogar donde a ${gato} es parte de la familia`;
+pRecibimos.innerHTML = `Recibir a ${gato} en nuestro hogar requiere de trabajo en equipo constante, por eso, cada quien tiene un roll que cumplir.`
+AyelenText.innerHTML = `Es la persona encargada de ser tu punto de contacto principal. Ella te mantendrá informado/a sobre cómo está pasando los días ${gato}, proporcionándote informacion detallada y actualizada. Ademas es creadora de contenido y quien se encarga de administra nuestras redes sociales, compartiendo fotos y videos adorables y divertidas de los michis pensionados.`
+
