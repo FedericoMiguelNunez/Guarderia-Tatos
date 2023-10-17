ff6036ee5b70920a4e5f00befaf73bf0246f88c0
FedericoMiguelNunez <fede24896@gmail.com>
2023-10-17 16:38:06 -0300

    fin

--- requisitos.js
+++ requisitos.js
@@ -8,6 +8,12 @@ const navBar = document.querySelector('.icon-bar');
 const nav = document.querySelector('.nav');
 const servicios = document.getElementById('servicios');
 const navQuienesSomos = document.querySelector('#Nav-Quienes_somos');
+const cambiarSub = document.getElementsByClassName('main-subtitulo')[0];
+const texto =  document.querySelectorAll('.texto');
+const ver = document.querySelectorAll('.ver')
+const aumentar = document.querySelectorAll('.aumentar')
+
+
 
 Home.addEventListener('click', function(){
     const HomeRedirigidos = 'Guarderia-Tatos.html' + '?gato=' + encodeURI(gato || '') + '&nombre=' + encodeURI(nombre || '');
@@ -26,6 +32,8 @@ navQuienesSomos.addEventListener('click', () => {
 
 if (gato != null && nombre != null && gato != "" && nombre != "") {
     cambio.innerHTML = `¡Requisitos que debe tener ${gato}!`;
+    cambiarSub.innerHTML = `En Guarderia Tatos sabemos que cuidar del bienestar de ${gato} es una prioridad tanto para ustedes cómo para nosotros. 
+    Para garantizar un ambiente sano y seguro cada pensionado debe cumplir con los siguientes requisitos sanitarios.`;
 
    
 
@@ -34,6 +42,7 @@ else {
     cambio.innerHTML = `¡Requisitos que debe tener tu Michi!`;
 
 }
+
 navBar.addEventListener('click', () => {
 
     nav.classList.toggle('active-nav')
@@ -45,3 +54,23 @@ window.addEventListener('scroll', () => {
         nav.classList.remove('active-nav');
     }
 });
+
+texto.forEach(texto=>{
+    texto.style.display = 'none'
+})
+ver.forEach((elemento)=>{
+    const texto = elemento.nextElementSibling;
+    const aumentar = elemento.parentElement;
+    elemento.addEventListener('click',()=>{
+if(texto.style.display === 'none' || texto.style.display === ''){
+    texto.style.display = 'block'
+    elemento.textContent = 'Ver menos';
+    aumentar.style.height ='750px'
+}
+else{
+    texto.style.display = 'none';
+      elemento.textContent = 'Ver más';
+      aumentar.style.height ='400px'
+}
+    })
+})
