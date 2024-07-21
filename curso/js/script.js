04fde8fad323da8ebb7bb5ffb379ea792fee1667
FedericoMiguelNunez <fede24896@gmail.com>
2024-07-21 12:57:59 -0300

    se redujo el video , sigue con el curso

--- curso/js/script.js
+++ curso/js/script.js
@@ -1,3 +1,16 @@
+const btnMostrarTemario = document.getElementById('ver-temario');
+
+btnMostrarTemario.addEventListener('click', () => {
+    const temario = document.querySelector('.temario');
+    if (temario.style.display === 'block') {
+        temario.style.display = 'none';
+        btnMostrarTemario.textContent = 'Ver temario';
+    } else {
+        temario.style.display = 'block';
+        btnMostrarTemario.textContent = 'Ocultar temario';
+    }
+
+});
 window.ityped.init(document.querySelector('.tipeado'),{
     strings:['Seguro', 'Estimulado', 'Sano', 'respetuoso'],
         loop : true
