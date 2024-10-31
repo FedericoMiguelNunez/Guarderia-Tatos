972c420a58ac41f6218f7a3f595df9fef9ab778f
FedericoMiguelNunez <fede24896@gmail.com>
2024-10-30 21:43:41 -0300

    seo.1.5

--- script-servicios.js
+++ script-servicios.js
@@ -9,20 +9,9 @@ const navQuienesSomos = document.querySelector('#Nav-Quienes_somos');
 const requisitos = document.getElementById("requisitos")
 const apuntamos = document.querySelector('.apuntamos')
 
-Home.addEventListener('click', function () {
-    const HomeRedirigidos = 'index.html' + '?gato=' + encodeURI(gato || '') + '&nombre=' + encodeURI(nombre || '');
-    window.location.href = HomeRedirigidos
-})
 
-navQuienesSomos.addEventListener('click', () => {
-    const QuienesSomosRedirigidos = 'Quienes-somos.html' + '?gato=' + encodeURI(gato || '') + '&nombre=' + encodeURI(nombre || '');
-    window.location.href = QuienesSomosRedirigidos;
-})
-requisitos.addEventListener('click', () => {
-    const RequisitosRedirigido = 'requisitos.html' + '?gato=' + encodeURI(gato || '') + '&nombre=' + encodeURI(nombre || '');
-    window.location.href = RequisitosRedirigido;
-}
-)
+
+
 navBar.addEventListener('click', () => {
 
     nav.classList.toggle('active-nav')
